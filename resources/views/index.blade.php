128ae8030de5bef5d1bf9d5eade64d5ffacac32d
Hibire01 <38668443+kaktv2016@users.noreply.github.com>
2026-09-14 14:14:30 +0700

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('title', 'หน้าแรก')
+@section('content')
+    <h1>หน้า Index eiei</h1>
+    <div class="alert alert-primary" role="alert">
+        This is a primary alert—check it out!
+    </div>
+    <p>hello word</p>
+@endsection
